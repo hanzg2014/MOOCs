6920f89fc9be8cde27e179249a112a5f6a789111
hanzg2014 <hanzg2014@gmail.com>
2017-04-08 15:50:43 +0900

    Add files via upload

---
+++ Udacity/ud730/QUIZ/Quiz_Softmax.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+import matplotlib.pyplot as plt
+import numpy as np
+# %matplotlib inline
+
+def softmax(x):
+# """Compute softmax values for each sets of scores in x."""
+#     pass  # TODO: Compute and return softmax(x)  
+    return np.exp(x) / np.sum(np.exp(x), axis = 0)
+
+scores = [3.0,1.0,2.0]
+
+if __name__ == '__main__':
+    print(softmax(scores))
+    
+    x = np.arange(-2.0, 6.0, 0.1)
+    scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])    #(v,h,d)
+    lines = plt.plot(x, softmax(scores).T, linewidth=2)
+    plt.title("Softmax([x,ones,ones])")
+    plt.xlabel("x")
+    plt.ylabel("Probability")
+    plt.legend(lines,["x", "ones1", "ones1"], loc = 7)
+    plt.show()
